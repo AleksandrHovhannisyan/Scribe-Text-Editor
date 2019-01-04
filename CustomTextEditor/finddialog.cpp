4c6c1d3f2583b3b59f0ef06b118a1e92f8ea1c9d
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-01-03 20:56:27 -0500

    Add FindDialog class for text searching.

---
+++ CustomTextEditor/finddialog.cpp
@@ -0,0 +1,49 @@
+#include "finddialog.h"
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QMessageBox>
+#include <QtDebug> // TODO remove
+
+/*
+ *
+ */
+FindDialog::FindDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    QLabel *findLabel = new QLabel(tr("Find what:"));
+    lineEdit = new QLineEdit();
+
+    findButton = new QPushButton(tr("&Find"));
+    queryText = "";
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->addWidget(findLabel);
+    layout->addWidget(lineEdit);
+    layout->addWidget(findButton);
+
+    setLayout(layout);
+    setWindowTitle(tr("Find"));
+
+    connect(findButton, SIGNAL(clicked()), this, SLOT(on_findButton_clicked()));
+}
+
+
+/*
+ *
+ */
+void FindDialog::on_findButton_clicked()
+{
+    QString query = lineEdit->text();
+
+    if(query.isEmpty())
+    {
+        QMessageBox::information(this, tr("Empty Field"), tr("Please enter a query."));
+        return;
+    }
+
+    queryText = query;
+    lineEdit->clear();
+
+    emit(queryTextReady(queryText));
+}
+
---
+++ CustomTextEditor/finddialog.h
@@ -0,0 +1,29 @@
+#ifndef FINDDIALOG_H
+#define FINDDIALOG_H
+#include <QDialog>
+#include <QString>
+#include <QLineEdit>
+#include <QPushButton>
+
+class FindDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    FindDialog(QWidget *parent = nullptr);
+    inline QString getQueryText(){ return queryText; }
+
+signals:
+    void queryTextReady(QString queryText);
+
+public slots:
+    void on_findButton_clicked();
+
+private:
+    QPushButton *findButton;
+    QLineEdit *lineEdit;
+    QString queryText;
+
+};
+
+#endif // FINDDIALOG_H
