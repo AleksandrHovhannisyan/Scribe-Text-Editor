cc5f359fc4f4f8258deced32a1eb8d027763e2d9
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-01-06 13:04:33 -0500

    Add more document metrics and insert TODO for future correction.

--- CustomTextEditor/documentmetrics.h
+++ CustomTextEditor/documentmetrics.h
@@ -3,10 +3,13 @@
 
 struct DocumentMetrics
 {
-    DocumentMetrics() : charCount(0), wordCount(0), lineCount(1) {}
+    DocumentMetrics() : charCount(0), wordCount(0), lineCount(1),
+                        positionOfLastFindMatch(-1), currentColumn(1) {}
     int charCount;
     int wordCount;
-    int lineCount;
+    int lineCount; // TODO remove
+    int positionOfLastFindMatch;
+    int currentColumn;
 };
 
 #endif // DOCUMENTMETRICS_H
