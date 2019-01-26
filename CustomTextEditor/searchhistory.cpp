58e7be86de340e5f617e7366c4a47d260e64c4e2
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-01-26 09:50:35 -0500

    Document SearchHistory add function.

--- CustomTextEditor/searchhistory.cpp
+++ CustomTextEditor/searchhistory.cpp
@@ -1,7 +1,9 @@
 #include "searchhistory.h"
 
 
-/* TODO document
+/* Records the given search term and its information in the current search history chain.
+ * If the term starts a new chain, then the search history is cleared before the term is inserted.
+ * Effectively, this means there will only ever be one item in the search history at any given point.
  */
 void SearchHistory::add(QString term, int cursorPositionBeforeFirstSearch, int firstFoundAt)
 {
